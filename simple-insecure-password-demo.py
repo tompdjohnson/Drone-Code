899d6e31ab42c6206a668e5a034866d58ec2a15a
tompdjohnson <tompdjohnson@outlook.com>
2020-09-29 11:55:13 +0100

    Create simple-insecure-password-demo.py

---
+++ simple-insecure-password-demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# ---------------------------------------------
+# Simple insecure password code - Acon1tum 2020
+# ---------------------------------------------
+
+# defining variable
+
+password = "letmein"
+
+
+# simple if/else statement 
+
+if password == "letmein":
+	print ("Correct")
+
+else:
+	print ("Access Denied")
+
